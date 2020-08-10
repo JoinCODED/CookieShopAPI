aa409600c00194b4a3933bf64d5d77175267e0a9
Lailz <lailaabdulraheem@gmail.com>
2020-08-10 18:39:31 +0300

    add token

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRATION_MS, JWT_SECRET } = require("../config/keys");
 
 // Models
 const { User } = require("../db/models");
@@ -8,8 +10,8 @@ exports.signup = async (req, res, next) => {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     req.body.password = hashedPassword;
-    await User.create(req.body);
-    res.status(201).json({ message: "User created successfully" });
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
   } catch (error) {
     next(error);
   }
@@ -17,4 +19,16 @@ exports.signup = async (req, res, next) => {
 
 exports.signin = async (req, res, next) => {
   console.log("exports.signin -> req", req.user);
+  const { user } = req;
+  const payload = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    role: user.role,
+    expires: Date.now() + JWT_EXPIRATION_MS,
+  };
+  const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+  res.json({ token });
 };
