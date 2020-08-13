cea726a85c6951139acd6634dbc5c91a411fe03f
Lailz <lailaabdulraheem@gmail.com>
2020-08-13 13:33:56 +0300

    add db url

--- db/db.js
+++ db/db.js
@@ -16,7 +16,8 @@ const db = new Sequelize({
   dialect: "postgres",
   host: "localhost",
   logging: false,
-  use_env_variable: "DATABASE_URL",
+  use_env_variable:
+    "postgres://oxpasnosmdvhwz:a2337be6ba44e6b91e7719a96dda9069ea2fb7e8cd4b5dbb4cb07de8a67c5d71@ec2-34-225-162-157.compute-1.amazonaws.com:5432/d83u9jl6htstvr",
 });
 
 module.exports = db;
