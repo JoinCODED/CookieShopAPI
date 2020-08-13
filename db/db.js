01f2697576d8f16187c3acee8c619737ee5db590
Lailz <lailaabdulraheem@gmail.com>
2020-08-13 13:39:12 +0300

    ذمثشرعح

--- db/db.js
+++ db/db.js
@@ -14,10 +14,8 @@ const db = new Sequelize({
   password: "password",
   database: "cookieshop_db",
   dialect: "postgres",
-  host: "localhost",
   logging: false,
-  use_env_variable:
-    "postgres://oxpasnosmdvhwz:a2337be6ba44e6b91e7719a96dda9069ea2fb7e8cd4b5dbb4cb07de8a67c5d71@ec2-34-225-162-157.compute-1.amazonaws.com:5432/d83u9jl6htstvr",
+  use_env_variable: "DATABASE_URL",
 });
 
 module.exports = db;
