1c5c86135e624d92fa2767086293249964fa0623
Lailz <lailaabdulraheem@gmail.com>
2020-08-13 13:43:19 +0300

    test

--- db/db.js
+++ db/db.js
@@ -10,12 +10,22 @@ const { Sequelize } = require("sequelize");
 // });
 
 const db = new Sequelize({
-  username: "postgres",
-  password: "password",
-  database: "cookieshop_db",
-  dialect: "postgres",
-  logging: false,
-  use_env_variable: "DATABASE_URL",
+  development: {
+    username: "postgres",
+    password: "password",
+    database: "cookieshop_db",
+    dialect: "postgres",
+    host: "localhost",
+    logging: false,
+  },
+  production: {
+    username: "postgres",
+    password: "password",
+    database: "cookieshop_db",
+    dialect: "postgres",
+    logging: false,
+    use_env_variable: "DATABASE_URL",
+  },
 });
 
 module.exports = db;
